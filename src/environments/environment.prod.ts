c8b641db5b73416ff73bdcfa91ac26ef3d81bed3
CoderGuy614 <jlutz.110@gmail.com>
2021-09-26 12:08:57 -0400

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase:  {
-    apiKey: "AIzaSyAlhPbOgeKxwurLG4FwaOl2fXFNCIVN2ck",
-    authDomain: "sharestream-62d6a.firebaseapp.com",
-    projectId: "sharestream-62d6a",
-    storageBucket: "sharestream-62d6a.appspot.com",
-    messagingSenderId: "1013568546238",
-    appId: "1:1013568546238:web:02f4b8d50f0ed5ada7c5ef",
-    measurementId: "G-YCJL4LL1BH"
+    apiKey: "AIzaSyBM2qShyX6K_3qmXer9f5KHUAhlX-O904E",
+    authDomain: "angularfirematerial.firebaseapp.com",
+    projectId: "angularfirematerial",
+    storageBucket: "angularfirematerial.appspot.com",
+    messagingSenderId: "96860960143",
+    appId: "1:96860960143:web:c5c61a2d31d616ee0587fa",
+    measurementId: "G-658W8TXGEZ"
   }
 };
